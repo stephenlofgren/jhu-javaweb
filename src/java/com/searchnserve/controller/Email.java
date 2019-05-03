dc6a09e3a74738e8c4ec0894bc9afad561bdfc74
bertramrichardson <bertram_richardson@yahoo.com>
2019-05-03 00:18:00 -0400

    email
    add email capabilities may need testing on a configured server

---
+++ src/java/com/searchnserve/controller/Email.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.searchnserve.controller;
+
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Email
+ */
+@WebServlet(name = "Email", urlPatterns = {"/Email"})
+public class Email extends HttpServlet {
+    String name = null, email = null, title = null;
+    String from = "bricha32jhu@gmail.com";
+    String subject = "Search N Serve Event Confirmation";
+    boolean isBodyHTML = false;
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession();
+        ServletContext servletContext = getServletContext();
+        PrintWriter out = response.getWriter();
+            try {
+                email = request.getParameter("email");
+                name = request.getParameter("name");
+                title = request.getParameter("title");
+                String body = "Dear " + name + ",\n\n"
+                + "Thanks for adding" + title + " and helping save the world. We'll make sure to send "
+                + "reminders about future events in your area.\n"
+                + "Have a great day and thanks again!\n\n"
+                + "Bertram Richardson\n"
+                + "Search N' Serve";
+                Gmail.sendMail(email, from, subject, body, isBodyHTML);
+                getServletContext().getRequestDispatcher("/HomeController").forward(request, response);
+            } catch (MessagingException e) {
+                String errorMessage
+                        = "ERROR: Unable to send email. "
+                        + "Check Tomcat logs for details.<br>"
+                        + "NOTE: You may need to configure your system "
+                        + "as described in chapter 14.<br>"
+                        + "ERROR MESSAGE: " + e.getMessage();
+                session.setAttribute("message", errorMessage); 
+                getServletContext().getRequestDispatcher("/AboutController").forward(request, response);
+            }
+        finally
+        {
+            out.close();
+        }
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
---
+++ src/java/com/searchnserve/controller/Gmail.java
@@ -0,0 +1,41 @@
+package com.searchnserve.controller;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class Gmail {
+
+    public static void sendMail(String to, String from,
+            String subject, String body, boolean bodyIsHTML)
+            throws MessagingException {
+        
+        // 1 - get a mail session
+        Properties props = new Properties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", 587);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.quitwait", "false");
+        Session session = Session.getDefaultInstance(props);
+        session.setDebug(true);
+
+        // 2 - create a message
+        Message message = new MimeMessage(session);
+        message.setSubject(subject);
+        message.setText(body);
+       
+        // 3 - address the message
+        Address fromAddress = new InternetAddress(from);
+        Address toAddress = new InternetAddress(to);
+        message.setFrom(fromAddress);
+        message.setRecipient(Message.RecipientType.TO, toAddress);
+
+        // 4 - send the message
+        Transport transport = session.getTransport();
+        transport.connect("bricha32jhu@gmail.com", "bertricha$jhu");
+        transport.sendMessage(message, message.getAllRecipients());
+        transport.close();
+    }
+}
