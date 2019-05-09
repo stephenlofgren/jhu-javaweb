4cb14f02341ee5dd9b2e14527e1d453c419c6307
jjose1384 <jjose1384@gmail.com>
2019-05-09 13:34:11 -0400

    Updates UserAccount to include phone and address and updated Signup screen to include those fields

--- src/java/com/searchnserve/controller/SignupController.java
+++ src/java/com/searchnserve/controller/SignupController.java
@@ -8,9 +8,9 @@ package com.searchnserve.controller;
 import com.searchnserve.data.UserDB;
 import com.searchnserve.model.UserAccount;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -36,9 +36,10 @@ public class SignupController extends HttpServlet {
             throws ServletException, IOException {
 
         HttpSession session = request.getSession();
-        String message = "";
-        String returnUri = "/signup.jsp";   
+        String error = "";
+        String returnUri = "/signup.jsp";
         boolean addUser = "Join Now".equals(request.getParameter("action"));
+        ArrayList<String> messages = new ArrayList<>();
 
         // create new UserAccount object for add, or retrieve from session for
         // edit
@@ -50,39 +51,37 @@ public class SignupController extends HttpServlet {
         // email is different from what's saved for the user
         if (addUser
                 || (!addUser && !user.getEmailAddress().equalsIgnoreCase(request.getParameter("email")))) {
-            
+
             if (UserDB.selectUserByEmail(request.getParameter("email")) != null) {
-                message = "Provided email address already exists.<br/>";
+                error = "Provided email address already exists.<br/>";
             } else {
                 user.setEmailAddress(request.getParameter("email"));
             }
-            
+
+        }
+        
+        validateData(request, user, messages);
+        if (!"".equals(error)){
+            messages.add(error);
         }
 
-        // validate that password and password confirmation match
-        if (!request.getParameter("password").equals(
-                request.getParameter("confirmPassword"))) {
-            message += "Provided passwords don't match.";
-        } else {
-            user.setPasswordHash(request.getParameter("password"));
-        }
 
-        // if message is empty there are no validation errors, and data should
+        // if error is empty there are no validation errors, and data should
         // be saved in the db
-        if ("".equals(message)) {
+        if (messages.isEmpty()) {
             if (addUser) {
                 if (UserDB.createAccount(user) == false) { // create account
-                    message = "Account could not be created.";
+                    messages.add("Account could not be created.");
                 }
             } else {
                 if (UserDB.updateAccount(user) == false) { // update account
-                    message = "Account could not be updated.";
+                    messages.add("Account could not be updated.");
                 }
             }
         }
 
-        // if message is still empty, then data was successfuly saved in the db
-        if ("".equals(message)) {
+        // if error is still empty, then data was successfuly saved in the db
+        if (messages.isEmpty()) {
             // place new or updated user into session
             session.setAttribute("userAccount", user);
             if (request.getAttribute("returnUri") == null) {
@@ -92,15 +91,115 @@ public class SignupController extends HttpServlet {
                 request.removeAttribute("returnUri");
             }
         } else {
-            // add message to request
+            // add error to request
+            String message = "";
+            for (String errorMessage: messages){
+                message += errorMessage + "<br/>";
+            }
+            
             request.setAttribute("message", message);
             // add temporary user to request so form values are preserved
             request.setAttribute("userAccount", user);
-        } 
-        
-        // navigate to the signup screen
-        response.sendRedirect(request.getContextPath() + returnUri);   
+        }
 
+        // navigate to the signup screen
+//        response.sendRedirect(request.getContextPath() + returnUri);
+        request.getRequestDispatcher(returnUri).forward(request, response);
+
+    }
+
+    /**
+     * Validates submitted data and populates the user object. Returns true if
+     * data is valid and false otherwise. Validation error messages will be
+     * populated in the messages ArrayList
+     *
+     * @param user
+     * @param messages
+     * @return
+     */
+    private boolean validateData(HttpServletRequest request,
+            UserAccount user, ArrayList<String> messages) {
+        boolean validationSuccessful = true;
+
+        // full name
+        user.setName(nullToEmpty(request.getParameter("fullName")));
+        if ("".equals(user.getName())) {
+            validationSuccessful = false;
+            messages.add("Full name is required.");
+        }
+
+        // email
+        user.setEmailAddress(nullToEmpty(request.getParameter("email")));
+        if ("".equals(user.getEmailAddress())) {
+            validationSuccessful = false;
+            messages.add("Email is required.");
+        }
+
+        // password
+        user.setPasswordHash(nullToEmpty(request.getParameter("password")));
+        if ("".equals(user.getPasswordHash())) {
+            validationSuccessful = false;
+            messages.add("Password is required.");
+        }
+        
+        if (user.getPasswordHash().length() < 10) {
+            validationSuccessful = false;
+            messages.add("Password must be at least 10 characters long.");
+        }
+
+        if ("".equals(nullToEmpty(request.getParameter("confirmPassword")))) {
+            validationSuccessful = false;
+            messages.add("Password Confirmation is required.");
+        }
+
+        if (!user.getPasswordHash().equals(nullToEmpty(request.getParameter("confirmPassword")))) {
+            validationSuccessful = false;
+            messages.add("Provided passwords must match.");
+        }
+
+        // phone number
+        user.setPhoneNumber(nullToEmpty(request.getParameter("phoneNumber")));
+        try {
+            if (!"".equals(user.getPhoneNumber())){
+                Long.parseLong(user.getPhoneNumber());
+            }
+
+        } catch (NumberFormatException nfe) {
+
+            validationSuccessful = false;
+            messages.add("Phone number cannot contain non-digit characters");
+        }
+        
+        // address line 1
+        user.setAddressLine1(nullToEmpty(request.getParameter("addressLine1")));
+        
+        // address line 2
+        user.setAddressLine2(nullToEmpty(request.getParameter("addressLine2")));
+        
+        // city
+        user.setCity(nullToEmpty(request.getParameter("city")));
+        
+        // state
+        user.setState(nullToEmpty(request.getParameter("state")));
+        
+        // zip code
+        user.setZipCode(nullToEmpty(request.getParameter("zipCode")));
+        try {
+            if (!"".equals(user.getZipCode())){
+                Long.parseLong(user.getZipCode());
+            }
+
+        } catch (NumberFormatException nfe) {
+
+            validationSuccessful = false;
+            messages.add("Zip Code cannot contain non-digit characters");
+        }
+
+        return validationSuccessful;
+    }
+
+    private String nullToEmpty(String string) {
+        return string == null ? "" : string.trim();
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
--- src/java/com/searchnserve/model/UserAccount.java
+++ src/java/com/searchnserve/model/UserAccount.java
@@ -29,6 +29,12 @@ public class UserAccount implements Serializable {
     private String name;
     private String emailAddress;
     private String passwordHash;
+    private String phoneNumber;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String zipCode;
     @OneToMany(
         cascade = CascadeType.ALL,
         orphanRemoval = false
@@ -145,5 +151,53 @@ public class UserAccount implements Serializable {
             }
         }
         return false;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
     }
 }
